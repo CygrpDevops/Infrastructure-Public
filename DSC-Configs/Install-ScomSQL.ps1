69a4a041d9989062089131971b7983779156c3bd
Ashish Raj Srivastava <ashishrajsrivastava@yahoo.in>
2017-12-11 16:41:01 +0530

    upload scom configs

---
+++ DSC-Configs/Install-ScomSQL.ps1
@@ -0,0 +1,158 @@
+﻿Configuration Install-SQLBox
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$PackagePath,
+
+        [Parameter(Mandatory)]
+		[System.Management.Automation.PSCredential]$AdminCreds,
+		
+		[Parameter(Mandatory)]
+		[System.Management.Automation.PSCredential]$FileShareCreds,
+		
+		[Parameter(Mandatory)]
+		[System.Management.Automation.PSCredential]$SQLAgentCreds,
+		
+		[Parameter(Mandatory)]
+		[System.Management.Automation.PSCredential]$SQLServiceCreds,
+		
+		[Parameter(Mandatory)]
+		[System.Management.Automation.PSCredential]$SQLSAAccountCreds,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Features,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$UpdateSource,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$UpdateEnabled,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$InstallSharedDir,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$InstallSharedWOWDir,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$SQLInstanceName,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$SQLInstanceDir,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$SecurityMode,
+		
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$SQLSysAdminAccounts
+		
+
+    )
+	Import-DscResource -ModuleName xSQLServer,xWebAdministration
+			
+    Node localhost
+    {
+        
+
+        #
+        # Ensure that .NET framework features are installed (pre-reqs for SQL)
+        #
+       WindowsFeature "Web-WebServer"
+            {
+                Ensure = "Present"
+                Name = "Web-WebServer"
+            }
+
+            WindowsFeature "Web-Request-Monitor"
+            {
+                Ensure = "Present"
+                Name = "Web-Request-Monitor"
+            }
+
+            WindowsFeature "Web-Windows-Auth"
+            {
+                Ensure = "Present"
+                Name = "Web-Windows-Auth"
+            }
+
+            WindowsFeature "Web-Asp-Net"
+            {
+                Ensure = "Present"
+                Name = "Web-Asp-Net"
+            }
+
+            WindowsFeature "Web-Asp-Net45"
+            {
+                Ensure = "Present"
+                Name = "Web-Asp-Net45"
+            }
+
+            WindowsFeature "NET-WCF-HTTP-Activation45"
+            {
+                Ensure = "Present"
+                Name = "NET-WCF-HTTP-Activation45"
+            }
+
+            WindowsFeature "Web-Mgmt-Console"
+            {
+                Ensure = "Present"
+                Name = "Web-Mgmt-Console"
+            }
+
+            WindowsFeature "Web-Metabase"
+            {
+                Ensure = "Present"
+                Name = "Web-Metabase"
+            }
+         
+	    Log ParamLog
+        {
+            Message = "Running SQLInstall. PackagePath = $PackagePath"
+        }
+
+		
+		xSQLServerSetup SQLServerSetup
+
+		{           
+            SourcePath = $PackagePath
+            SourceCredential = $FileShareCreds           
+            PsDscRunAsCredential  = $AdminCreds
+			SQLSvcAccount = $SQLServiceCreds
+            AgtSvcAccount = $SQLAgentCreds
+			SAPWd = $SQLSAAccountCreds		
+			InstanceName =  $SQLInstanceName  
+			InstanceDir = $SQLInstanceDir  
+            SecurityMode =  $SecurityMode 
+            SQLSysAdminAccounts =  @($SQLSysAdminAccounts)
+			UpdateSource = $UpdateSource 
+     		InstallSharedDir = $InstallSharedDir
+            InstallSharedWOWDir = $InstallSharedWOWDir
+			Features = $Features 
+			UpdateEnabled =  $UpdateEnabled 
+        }
+        xSqlServerFirewall SqlFirewallRules
+        {
+                        DependsOn = '[xSQLServerSetup]SQLServerSetup'
+                        Ensure = "Present"
+                        SourcePath = $PackagePath
+                        InstanceName = $SQLInstanceName
+                        Features = $Features
+        }                       
+   
+		LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $True
+        }
+
+    }
+}
